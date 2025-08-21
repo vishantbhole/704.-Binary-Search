0124a4e048d4116427729e5fee290611a3049aee
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-20 20:34:49 -0500

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,2 +1,10 @@
 
 #704. Binary Search
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
